2aaa5f1559e09fc333a20a2e7477e82438282bbd
jfseb <jfsebpublic@gmail.com>
2017-03-14 22:34:18 +0100

    package proper

--- js/index.js
+++ js/index.js
@@ -5,13 +5,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //export * from "./match/ifmatch";
 var ErBase = require("./match/erbase");
 exports.ErBase = ErBase;
+var ErError = require("./match/ererror");
+exports.ErError = ErError;
 var Sentence = require("./match/sentence");
 exports.Sentence = Sentence;
 var InputFilterRules = require("./match/inputFilterRules");
 exports.InputFilterRules = InputFilterRules;
-var InputFilter = require("./match/inputFilterRules");
+var InputFilter = require("./match/inputFilter");
 exports.InputFilter = InputFilter;
 var Word = require("./match/word");
 exports.Word = Word;
+var IFErBase = require("./match/iferbase");
+exports.IFErBase = IFErBase;
 
 //# sourceMappingURL=index.js.map
